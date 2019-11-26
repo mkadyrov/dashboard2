0054b4bcd38c605b1635d6b9a9124a01d64a30f3
Max <max@kadyrov.me>
2019-11-26 18:36:48 +0600

    Updates

---
+++ precache-manifest.a9f172c5051e12aec956397a92d954dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a6f3449834941263377fcc6a0b68a19",
+    "url": "/dashboard2/index.html"
+  },
+  {
+    "revision": "03f5df17c21add41f4ba",
+    "url": "/dashboard2/static/css/main.b4b570d5.chunk.css"
+  },
+  {
+    "revision": "ddd5f2d4d20c1f7b4900",
+    "url": "/dashboard2/static/js/2.06074171.chunk.js"
+  },
+  {
+    "revision": "03f5df17c21add41f4ba",
+    "url": "/dashboard2/static/js/main.36088da9.chunk.js"
+  },
+  {
+    "revision": "cc7bf976f5383fa5630d",
+    "url": "/dashboard2/static/js/runtime-main.a919c673.js"
+  },
+  {
+    "revision": "90fb88312d0c67ab347a8435890c9e75",
+    "url": "/dashboard2/static/media/arrow-left.90fb8831.png"
+  },
+  {
+    "revision": "d24dd5b155c873cfa0204a3550cfbd28",
+    "url": "/dashboard2/static/media/star-gray.d24dd5b1.png"
+  }
+]);
